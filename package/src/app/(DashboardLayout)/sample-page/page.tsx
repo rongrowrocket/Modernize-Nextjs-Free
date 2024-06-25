641f99238d578e4f2fe045ba68d34f440f0693ff
rongrowrocket <ron@growrocket.net>
2024-06-25 21:48:15 +0100

    Update page.tsx

--- package/src/app/(DashboardLayout)/sample-page/page.tsx
+++ package/src/app/(DashboardLayout)/sample-page/page.tsx
@@ -8,7 +8,7 @@ const SamplePage = () => {
   return (
     <PageContainer title="Sample Page" description="this is Sample page">
       <DashboardCard title="Sample Page">
-        <Typography>This is a sample page</Typography>
+        <Typography>This is a sample page2</Typography>
       </DashboardCard>
     </PageContainer>
   );
